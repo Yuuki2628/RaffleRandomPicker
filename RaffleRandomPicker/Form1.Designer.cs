d36431b54c9422b35877abcde1beb9ab7bbb2ab3
Yuuki26268 <singhrobin2628@gmail.com>
2021-05-06 13:09:58 +0200

    Aggiungere i file di progetto.

---
+++ RaffleRandomPicker/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace RaffleRandomPicker
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnGetRandom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(12, 12);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(113, 36);
+            this.btnReadFile.TabIndex = 0;
+            this.btnReadFile.Text = "Read a file";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 54);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(338, 268);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // btnGetRandom
+            // 
+            this.btnGetRandom.Location = new System.Drawing.Point(237, 12);
+            this.btnGetRandom.Name = "btnGetRandom";
+            this.btnGetRandom.Size = new System.Drawing.Size(113, 36);
+            this.btnGetRandom.TabIndex = 2;
+            this.btnGetRandom.Text = "End raffle";
+            this.btnGetRandom.UseVisualStyleBackColor = true;
+            this.btnGetRandom.Click += new System.EventHandler(this.btnGetRandom_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 334);
+            this.Controls.Add(this.btnGetRandom);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnReadFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FrmMain";
+            this.ShowIcon = false;
+            this.Text = "Random Picker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnGetRandom;
+    }
+}
+
---
+++ RaffleRandomPicker/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RaffleRandomPicker
+{
+    public partial class FrmMain : Form
+    {
+        int entriesNumber = 0;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(openFile.FileName);
+                    string line;
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        int entries;
+                        int.TryParse(line.Substring(0, line.IndexOf('-')), out entries);
+                        if(entries > 0)
+                        {
+                            string user = line.Substring(line.IndexOf('-') + 1);
+                            for (int i = 0; i < entries; i++)
+                            {
+                                listView1.Items.Add(user);
+                                entriesNumber++;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error message: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnGetRandom_Click(object sender, EventArgs e)
+        {
+            Random g = new Random();
+            string winner;
+            int winningNumber = g.Next(0, entriesNumber - 1);
+            winner = listView1.Items[winningNumber].Text;
+            MessageBox.Show("The winner is: " + winner, "Congratulations " + winner);
+        }
+    }
+}
